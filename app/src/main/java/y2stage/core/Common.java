94f6945eaa2420512ceca5f965ab646c6423a3d1
ytowstage <ytowstage@gmail.com>
2015-04-14 12:50:27 +0900

    log 커스텀

--- app/src/main/java/y2stage/core/Common.java
+++ app/src/main/java/y2stage/core/Common.java
@@ -9,11 +9,45 @@ package y2stage.core;
 import android.content.Context;
 import android.os.CountDownTimer;
 import android.widget.Toast;
+import android.util.Log;
 
 import y2stage.core.helper.StringUtil;
 
 
+
 public class Common {
+
+    public final static String LOG_TAG="Y2STAGE";
+
+    /*---------------------------------------------------------------
+	 @brief  : 로그 커스텀 (차후 디버그 내용 서버로 전달)
+	 @parameters : Context
+	 			   msg :  메세지
+	 @return : bool
+	---------------------------------------------------------------*/
+    public void log(String msg){
+        this.log(msg,"d");
+    }
+
+    public void log(String msg, String i){
+        switch (i){
+            case "v":
+                Log.i(LOG_TAG,msg);
+                break;
+            case "d":
+                Log.i(LOG_TAG,msg);
+                break;
+            case "i":
+                Log.i(LOG_TAG,msg);
+                break;
+            case "w":
+                Log.i(LOG_TAG,msg);
+                break;
+            case "e":
+                Log.i(LOG_TAG,msg);
+                break;
+        }
+    }
 
     /*---------------------------------------------------------------
 	 @brief  : 토스트 커스텀
